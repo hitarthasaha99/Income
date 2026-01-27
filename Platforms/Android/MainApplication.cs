f70f62b976372333c777a94858d0ef1fc826a366
hitartha.saha <hitartha.saha@tuteck.com>
2026-01-27 21:51:19 +0530

    commit

--- Platforms/Android/MainApplication.cs
+++ Platforms/Android/MainApplication.cs
@@ -3,7 +3,7 @@ using Android.Runtime;
 
 namespace Income
 {
-    [Application(UsesCleartextTraffic = false)]
+    [Application(UsesCleartextTraffic = true)]
     public class MainApplication : MauiApplication
     {
         public MainApplication(IntPtr handle, JniHandleOwnership ownership)
